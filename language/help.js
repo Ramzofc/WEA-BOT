4f14b563c5adecc33004596dc270e5a14bd4d138
Ramzofc <rarfarter@gmail.com>
2022-05-20 19:06:42 +0800

    kaguraMD

---
+++ language/help.js
@@ -0,0 +1,959 @@
+exports.menu = (botname, pushname, salam, ownername, time, tanggal) => {
+	return`Selamat *${salam}* ${pushname} 👋
+Saya ${botname}, Bot Ini Adalah Beta Multi-Device WhatsApp.
+Jika Ada Fitur Error Atau Bug Segera Lapor Ke Owner Bot
+
+ ╭─❏「 *INFO BOT* 」
+ ├➣⬡ Creator : *RamzC*
+ ├➣⬡ Owner : *${ownername}*
+ ├➣⬡ Bot Name : *${botname}*
+ ├➣⬡ Tanggal : *${tanggal}*
+ ├➣⬡ Waktu : *${time}*
+ ├➣⬡ Prefix : *Multi Prefix*
+ ╰─━━━━━━━━━━━━━━❏
+  
+ ╭─⬡ *Grup Menu*
+ ├➣ #linkgroup
+ ├➣ #ephemeral [option]
+ ├➣ #setppgc
+ ├➣ #setname 
+ ├➣ #setdesc 
+ ├➣ #group [option]
+ ├➣ #editinfo [option]
+ ├➣ #add @user
+ ├➣ #kick @user
+ ├➣ #hidetag 
+ ├➣ #tagall 
+ ├➣ #antilink [on/off]
+ ├➣ #mute [on/off]
+ ├➣ #promote @user
+ ├➣ #demote @user
+ ├➣ #vote 
+ ├➣ #devote
+ ├➣ #upvote
+ ├➣ #cekvote
+ ├➣ #hapusvote
+ └❏
+  
+ ╭─⬡ *Owner Menu*
+ ├➣ #react [emoji]
+ ├➣ #chat [option]
+ ├➣ #join [link]
+ ├➣ #leave
+ ├➣ #block @user
+ ├➣ #unblock @user
+ ├➣ #bcgroup 
+ ├➣ #bcall 
+ ├➣ #setppbot  
+ ├➣ #setexif
+ ├➣ #setmenu
+ └❏
+ 
+ ╭─⬡ *Anonymous Menu*
+ ├➣ #anonymous
+ ├➣ #start
+ ├➣ #next
+ ├➣ #keluar
+ ├➣ #sendkontak
+ └❏
+ 
+ ╭─⬡ *Downloader Menu*
+ ├➣ #play
+ ├➣ #tiktoknowm 
+ ├➣ #tiktokwm 
+ ├➣ #tiktokmp3 
+ ├➣ #instagram 
+ ├➣ #twitter 
+ ├➣ #twittermp3 
+ ├➣ #facebook 
+ ├➣ #pinterestdl 
+ ├➣ #ytmp3 
+ ├➣ #ytmp4 
+ ├➣ #getmusic 
+ ├➣ #getvideo 
+ ├➣ #umma 
+ ├➣ #joox 
+ ├➣ #soundcloud 
+ └❏
+  
+ ╭─⬡ *Search Menu* 
+ ├➣ #yts 
+ ├➣ #google 
+ ├➣ #gimage 
+ ├➣ #pinterest 
+ ├➣ #wikimedia 
+ ├➣ #ytsearch 
+ ├➣ #ringtone 
+ ├➣ #stalk [option] 
+ ├➣ #webtoons 
+ ├➣ #drakor 
+ ├➣ #animesearch 
+ ├➣ #character 
+ ├➣ #manga 
+ ├➣ #playstore 
+ ├➣ #gsmarena 
+ ├➣ #jadwalbioskop 
+ ├➣ #nowplayingbioskop 
+ ├➣ #aminio 
+ ├➣ #wattpad 
+ ├➣ #webtoons 
+ ├➣ #drakor 
+ └❏
+  
+ ╭─⬡ *Telestick Menu*
+ ├➣ #gurastick
+ ├➣ #dogestick
+ ├➣ #bucinstick
+ ├➣ #patrick
+ └❏
+  
+ ╭─⬡ *Random Menu*
+ ├➣ #coffe
+ ├➣ #quotesanime
+ ├➣ #couple
+ ├➣ #darkjokes
+ └❏
+  
+ ╭─⬡ *Random Anime*
+ ├➣ #loli
+ ├➣ #neko
+ ├➣ #waifu
+ └❏
+  
+ ╭─⬡ *Textpro Menu*
+ ├➣ #3dchristmas
+ ├➣ #3ddeepsea
+ ├➣ #americanflag
+ ├➣ #3dscifi
+ ├➣ #3drainbow
+ ├➣ #3dwaterpipe
+ ├➣ #halloweenskeleton
+ ├➣ #sketch
+ ├➣ #bluecircuit
+ ├➣ #space
+ ├➣ #metallic
+ ├➣ #fiction
+ ├➣ #greenhorror
+ ├➣ #transformer
+ ├➣ #berry
+ ├➣ #thunder
+ ├➣ #magma
+ ├➣ #3dcrackedstone
+ ├➣ #3dneonlight
+ ├➣ #impressiveglitch
+ ├➣ #naturalleaves
+ ├➣ #fireworksparkle
+ ├➣ #matrix
+ ├➣ #dropwater
+ ├➣ #harrypotter
+ ├➣ #foggywindow
+ ├➣ #neondevils
+ ├➣ #christmasholiday
+ ├➣ #3dgradient
+ ├➣ #blackpink
+ ├➣ #gluetext
+ └❏
+  
+ ╭─⬡ *Photooxy Menu*
+ ├➣ #shadow
+ ├➣ #romantic
+ ├➣ #smoke
+ ├➣ #burnpapper
+ ├➣ #naruto
+ ├➣ #lovemsg
+ ├➣ #grassmsg
+ ├➣ #lovetext
+ ├➣ #coffecup
+ ├➣ #butterfly
+ ├➣ #harrypotter
+ ├➣ #retrolol
+ └❏
+  
+ ╭─⬡ *Ephoto Menu*
+ ├➣ #ffcover
+ ├➣ #crossfire
+ ├➣ #galaxy
+ ├➣ #glass
+ ├➣ #neon
+ ├➣ #beach
+ ├➣ #blackpink
+ ├➣ #igcertificate
+ ├➣ #ytcertificate
+ └❏
+  
+ ╭─⬡ *Games Menu*
+ ├➣ #simih
+ ├➣ #bagaimanakah
+ ├➣ #kapankah
+ ├➣ #apakah
+ ├➣ #bisakah
+ ├➣ #rate
+ ├➣ #wangy
+ ├➣ #gantengcek
+ ├➣ #cekganteng
+ ├➣ #cantikcek
+ ├➣ #cekcantik
+ ├➣ #sangecek
+ ├➣ #ceksange
+ ├➣ #gaycek
+ ├➣ #cekgay
+ ├➣ #lesbicek
+ ├➣ #halah
+ ├➣ #hilih
+ ├➣ #huluh
+ ├➣ #heleh
+ ├➣ #holoh
+ ├➣ #jadian
+ ├➣ #jodohku
+ ├➣ #tictactoe
+ ├➣ #delttt
+ ├➣ #family100
+ ├➣ #tebak [option]
+ ├➣ #math [mode]
+ ├➣ #suitpvp [@tag]
+ └❏
+
+ ╭─⬡ *Primbon Menu*
+ ├➣ #nomorhoki
+ ├➣ #artimimpi
+ ├➣ #artinama
+ ├➣ #ramaljodoh
+ ├➣ #ramaljodohbali
+ ├➣ #suamiistri
+ ├➣ #ramalcinta
+ ├➣ #cocoknama
+ ├➣ #pasangan
+ ├➣ #jadiannikah
+ ├➣ #sifatusaha
+ ├➣ #rezeki
+ ├➣ #pekerjaan
+ ├➣ #nasib
+ ├➣ #penyakit
+ ├➣ #tarot
+ ├➣ #fengshui
+ ├➣ #haribaik
+ ├➣ #harisangar
+ ├➣ #harisial
+ ├➣ #nagahari
+ ├➣ #arahrezeki
+ ├➣ #peruntungan
+ ├➣ #weton
+ ├➣ #karakter
+ ├➣ #keberuntungan
+ ├➣ #memancing
+ ├➣ #masasubur
+ ├➣ #zodiak
+ ├➣ #shio
+ └❏
+
+ ╭─⬡ *Cerpen Menu*
+ ├➣ #cerpen anak
+ ├➣ #cerpen bahasa daerah
+ ├➣ #cerpen bahasa inggris
+ ├➣ #cerpen bahasa jawa
+ ├➣ #cerpen bahasa sunda
+ ├➣ #cerpen budaya
+ ├➣ #cerpen cinta
+ ├➣ #cerpen cinta islami
+ ├➣ #cerpen cinta pertama
+ ├➣ #cerpen cinta romantis
+ ├➣ #cerpen cinta sedih
+ ├➣ #cerpen cinta segitiga
+ ├➣ #cerpen cinta sejati
+ ├➣ #cerpen galau
+ ├➣ #cerpen gokil
+ ├➣ #cerpen inspiratif
+ ├➣ #cerpen jepang
+ ├➣ #cerpen kehidupan
+ ├➣ #cerpen keluarga
+ ├➣ #cerpen kisah nyata
+ ├➣ #cerpen korea
+ ├➣ #cerpen kristen
+ ├➣ #cerpen liburan
+ ├➣ #cerpen lingkungan
+ ├➣ #cerpen lucu
+ ├➣ #cerpen malaysia
+ ├➣ #cerpen mengharukan
+ ├➣ #cerpen misteri
+ ├➣ #cerpen motivasi
+ ├➣ #cerpen nasihat
+ ├➣ #cerpen nasionalisme
+ ├➣ #cerpen olahraga
+ ├➣ #cerpen patah hati
+ ├➣ #cerpen penantian
+ ├➣ #cerpen pendidikan
+ ├➣ #cerpen pengalaman pribadi
+ ├➣ #cerpen pengorbanan
+ ├➣ #cerpen penyesalan
+ ├➣ #cerpen perjuangan
+ ├➣ #cerpen perpisahan
+ ├➣ #cerpen persahabatan
+ ├➣ #cerpen petualangan
+ ├➣ #cerpen ramadhan
+ ├➣ #cerpen remaja
+ ├➣ #cerpen renungan
+ ├➣ #cerpen rindu
+ ├➣ #cerpen rohani
+ ├➣ #cerpen romantis
+ ├➣ #cerpen sastra
+ ├➣ #cerpen sedih
+ ├➣ #cerpen sejarah
+ ├➣ #cerpen slice of life
+ ├➣ #cerpen terjemahan
+ ├➣ #cerpen thriller
+ └❏
+  
+ ╭─⬡ *Convert Menu*
+ ├➣ #toimage
+ ├➣ #removebg
+ ├➣ #sticker
+ ├➣ #emojimix
+ ├➣ #tovideo
+ ├➣ #togif
+ ├➣ #tourl
+ ├➣ #tovn
+ ├➣ #tomp3
+ ├➣ #toaudio
+ ├➣ #ebinary
+ ├➣ #dbinary
+ ├➣ #styletext
+ ├➣ #smeme
+ └❏
+
+ ╭─⬡ *Information Menu*
+ ├➣ #merdeka-news 
+ ├➣ #kontan-news 
+ ├➣ #cnbc-news 
+ ├➣ #tribun-news 
+ ├➣ #indozone-news 
+ ├➣ #kompas-news 
+ ├➣ #detik-news 
+ ├➣ #daily-news 
+ ├➣ #inews-news 
+ ├➣ #okezone-news 
+ ├➣ #sindo-news 
+ ├➣ #tempo-news 
+ ├➣ #antara-news 
+ ├➣ #cnn-news 
+ ├➣ #fajar-news 
+ └❏
+  
+ ╭─⬡ *Main Menu*
+ ├➣ #script
+ ├➣ #ping
+ ├➣ #runtime
+ ├➣ #speed
+ ├➣ #owner
+ ├➣ #menu
+ ├➣ #delete
+ ├➣ #infochat
+ ├➣ #quoted
+ ├➣ #listpc
+ ├➣ #listgc
+ ├➣ #listonline
+ └❏
+  
+ ╭─⬡ *Database Menu*
+ ├➣ #setcmd
+ ├➣ #listcmd
+ ├➣ #delcmd
+ ├➣ #lockcmd
+ ├➣ #addmsg
+ ├➣ #listmsg
+ ├➣ #getmsg
+ ├➣ #delmsg
+ └❏
+  
+ ╭─⬡ *Islamic Menu*
+ ├➣ #iqra
+ ├➣ #hadist
+ ├➣ #alquran
+ ├➣ #juzamma
+ ├➣ #tafsirsurah
+ └❏
+  
+ ╭─⬡ *Voice Changer*
+ ├➣ #bass
+ ├➣ #blown
+ ├➣ #deep
+ ├➣ #earrape
+ ├➣ #fast
+ ├➣ #fat
+ ├➣ #nightcore
+ ├➣ #reverse
+ ├➣ #robot
+ ├➣ #slow
+ ├➣ #tupai`
+ 
+}
+exports.menusimpel = (botname, pushname, salam, ownername, time, tanggal) => {
+	return`Selamat *${salam}* ${pushname} 👋
+
+  *INFO BOT*
+  👑 Creator : *RamzC*
+  👤 Owner : *${ownername}*
+  🤖 Bot Name : *${botname}*
+  📑 Tanggal : *${tanggal}*
+  ⏰ Waktu : *${time}*
+  🗒 ️Prefix : *Multi Prefix*
+
+  > *MENU SIMPEL*
+  
+  ❏ #menu1 _( groupmenu )_
+  ❏ #menu2 _( downloadmenu )_
+  ❏ #menu3 _( searchmenu )_
+  ❏ #menu4 _( telestickmenu )_
+  ❏ #menu5 _( randommenu )_
+  ❏ #menu6 _( animemenu )_
+  ❏ #menu7 _( searchmenu )_
+  ❏ #menu8 _( textpromenu )_
+  ❏ #menu9 _( photomenu )_
+  ❏ #menu10 _( ephotomenu )_
+  ❏ #menu11 _( funmenu )_
+  ❏ #menu12 _( primbonmenu )_
+  ❏ #menu13 _( cerpenmenu )_
+  ❏ #menu14 _( convertmenu )_
+  ❏ #menu15 _( informasimenu )_
+  ❏ #menu16 _( mainmenu )_
+  ❏ #menu17 _( databasemenu )_
+  ❏ #menu18 _( anonymousmenu )_
+  ❏ #menu19 _( islamicmenu )_
+  ❏ #menu20 _( voicemenu )_
+  ❏ #menu21 _( ownermenu )_
+
+Saya ${botname}, Bot Ini Adalah Beta Multi-Device WhatsApp.
+Jika Ada Fitur Error Atau Bug Segera Lapor Ke Owner Bot.`
+}
+
+//—————「 List 」—————//
+exports.list = (pushname) => {
+	return`Halo kak ${pushname}, silahkan dipilih list menu bot dibawah ini, harap kasih jeda 5 detik sebelum menggunakan fitur lainnya.`
+}
+//—————「 Welcome 」—————//
+exports.welcome = () => {
+	return`Welcome
+`
+}
+
+//—————「 Leave 」—————//
+exports.leave = () => {
+	return`Byeee
+`
+}
+
+//—————「 Donasi 」—————//
+exports.donasi  = (ownernomer) => {
+	return`
+—————「 *DONATE* 」—————
+
+Hai kak ☺️ 
+Kalian bisa mendukung saya agar bot ini tetap up to date dengan cara donasi
+Berapapun donasi kalian akan sangat berarti 👍
+
+Thanks!
+
+—————「 *SEWA | RUN BOT* 」—————
+
+  *_SEWA BOT_* 
+  *❏* Sewa Bot 15k (1 minggu)
+  *❏* Sewa Bot 20k (1 bulan)
+  *❏* Sewa Bot 25k (Permanen)
+
+  *JADI BOT* 
+  *❏* Jadi Bot 15k Tidak Owner (1 minggu)
+  *❏* Jadi Bot + Owner 20k (1 bulan)
+  *❏* Jadi Bot + Owner 25k (Permanen)
+
+  *Minat? Langsung chat aja ya bg😅🙏*
+
+  *Contact person Owner* :
+  wa.me/${ownernomer}
+`
+}
+
+exports.groupmenu = (prefix) => {
+	return`
+  *GROUP MENU*
+  *≻* ${prefix}linkgroup
+  *≻* ${prefix}ephemeral [option]
+  *≻* ${prefix}setppgc  
+  *≻* ${prefix}setname 
+  *≻* ${prefix}setdesc 
+  *≻* ${prefix}group [option]
+  *≻* ${prefix}editinfo [option]
+  *≻* ${prefix}add @user
+  *≻* ${prefix}kick @user
+  *≻* ${prefix}hidetag 
+  *≻* ${prefix}tagall 
+  *≻* ${prefix}antilink [on/off]
+  *≻* ${prefix}mute [on/off]
+  *≻* ${prefix}promote @user
+  *≻* ${prefix}demote @user
+  *≻* ${prefix}vote 
+  *≻* ${prefix}devote
+  *≻* ${prefix}upvote
+  *≻* ${prefix}cekvote
+  *≻* ${prefix}hapusvote
+  `
+}
+
+exports.downloadermenu = (prefix) => {
+	return`
+  *DOWNLOADER MENU*
+  *≻* ${prefix}tiktoknowm 
+  *≻* ${prefix}tiktokwm 
+  *≻* ${prefix}tiktokmp3 
+  *≻* ${prefix}instagram 
+  *≻* ${prefix}twitter 
+  *≻* ${prefix}twittermp3 
+  *≻* ${prefix}facebook 
+  *≻* ${prefix}pinterestdl 
+  *≻* ${prefix}ytmp3 
+  *≻* ${prefix}ytmp4 
+  *≻* ${prefix}getmusic 
+  *≻* ${prefix}getvideo 
+  *≻* ${prefix}umma 
+  *≻* ${prefix}joox 
+  *≻* ${prefix}soundcloud 
+  `
+}
+
+exports.searchmenu = (prefix) => {
+	return`
+  *SEARCH MENU*
+  *≻* ${prefix}play 
+  *≻* ${prefix}yts 
+  *≻* ${prefix}google 
+  *≻* ${prefix}gimage 
+  *≻* ${prefix}pinterest 
+  *≻* ${prefix}wikimedia 
+  *≻* ${prefix}ytsearch 
+  *≻* ${prefix}ringtone 
+  *≻* ${prefix}stalk [option] 
+  *≻* ${prefix}webtoons 
+  *≻* ${prefix}drakor 
+  *≻* ${prefix}animesearch 
+  *≻* ${prefix}character 
+  *≻* ${prefix}manga 
+  *≻* ${prefix}playstore 
+  *≻* ${prefix}gsmarena 
+  *≻* ${prefix}jadwalbioskop 
+  *≻* ${prefix}nowplayingbioskop 
+  *≻* ${prefix}aminio 
+  *≻* ${prefix}wattpad 
+  *≻* ${prefix}webtoons 
+  *≻* ${prefix}drakor 
+  `
+}
+
+exports.telestickmenu = (prefix) => {
+	return`
+  *TELE STICK MENU*
+  *≻* ${prefix}gurastick
+  *≻* ${prefix}dogestick
+  *≻* ${prefix}bucinstick
+  *≻* ${prefix}patrick
+`
+}
+
+exports.randommenu = (prefix) => {
+	return`
+  *RANDOM MENU*
+  *≻* ${prefix}coffe
+  *≻* ${prefix}quotesanime
+  *≻* ${prefix}couple
+  *≻* ${prefix}darkjokes
+  `
+}
+
+exports.randomanimemenu = (prefix) => {
+	return`
+  *RANDOM ANIME MENU*
+  *≻* ${prefix}loli
+  *≻* ${prefix}neko
+  *≻* ${prefix}waifu
+  *≻* ${prefix}shinobu
+  *≻* ${prefix}megumin
+  *≻* ${prefix}bully
+  *≻* ${prefix}cuddle
+  *≻* ${prefix}cry
+  *≻* ${prefix}hug
+  *≻* ${prefix}awoo
+  *≻* ${prefix}kiss
+  *≻* ${prefix}lick
+  *≻* ${prefix}pat
+  *≻* ${prefix}smug
+  *≻* ${prefix}bonk
+  *≻* ${prefix}yeet
+  *≻* ${prefix}blush
+  *≻* ${prefix}smile
+  *≻* ${prefix}wave
+  *≻* ${prefix}highfive
+  *≻* ${prefix}handhold
+  *≻* ${prefix}nom
+  *≻* ${prefix}bite
+  *≻* ${prefix}glomp
+  *≻* ${prefix}slap
+  *≻* ${prefix}kill
+  *≻* ${prefix}happy
+  *≻* ${prefix}wink
+  *≻* ${prefix}poke
+  *≻* ${prefix}dance
+  *≻* ${prefix}cringe
+  *≻* ${prefix}anime
+  *≻* ${prefix}waifu
+  *≻* ${prefix}husbu
+  *≻* ${prefix}neko
+  *≻* ${prefix}shinobu
+  *≻* ${prefix}waifus 
+  *≻* ${prefix}nekos 
+  *≻* ${prefix}trap 
+  *≻* ${prefix}blowjob 
+  `
+}
+
+exports.textpromenu = (prefix) => {
+	return`
+  *TEXT PRO MENU*
+  *≻* ${prefix}3dchristmas
+  *≻* ${prefix}3ddeepsea
+  *≻* ${prefix}americanflag
+  *≻* ${prefix}3dscifi
+  *≻* ${prefix}3drainbow
+  *≻* ${prefix}3dwaterpipe
+  *≻* ${prefix}halloweenskeleton
+  *≻* ${prefix}sketch
+  *≻* ${prefix}bluecircuit
+  *≻* ${prefix}space
+  *≻* ${prefix}metallic
+  *≻* ${prefix}fiction
+  *≻* ${prefix}greenhorror
+  *≻* ${prefix}transformer
+  *≻* ${prefix}berry
+  *≻* ${prefix}thunder
+  *≻* ${prefix}magma
+  *≻* ${prefix}3dcrackedstone
+  *≻* ${prefix}3dneonlight
+  *≻* ${prefix}impressiveglitch
+  *≻* ${prefix}naturalleaves
+  *≻* ${prefix}fireworksparkle
+  *≻* ${prefix}matrix
+  *≻* ${prefix}dropwater
+  *≻* ${prefix}harrypotter
+  *≻* ${prefix}foggywindow
+  *≻* ${prefix}neondevils
+  *≻* ${prefix}christmasholiday
+  *≻* ${prefix}3dgradient
+  *≻* ${prefix}blackpink
+  *≻* ${prefix}gluetext
+  `
+}
+
+exports.photooxymenu = (prefix) => {
+	return`
+  *PHOTO OXY MENU*
+  *≻* ${prefix}shadow
+  *≻* ${prefix}romantic
+  *≻* ${prefix}smoke
+  *≻* ${prefix}burnpapper
+  *≻* ${prefix}naruto
+  *≻* ${prefix}lovemsg
+  *≻* ${prefix}grassmsg
+  *≻* ${prefix}lovetext
+  *≻* ${prefix}coffecup
+  *≻* ${prefix}butterfly
+  *≻* ${prefix}harrypotter
+  *≻* ${prefix}retrolol
+  `
+}
+
+exports.ephotomenu = (prefix) => {
+	return`
+  *EPHOTO MENU*
+  *≻* ${prefix}ffcover
+  *≻* ${prefix}crossfire
+  *≻* ${prefix}galaxy
+  *≻* ${prefix}glass
+  *≻* ${prefix}neon
+  *≻* ${prefix}beach
+  *≻* ${prefix}blackpink
+  *≻* ${prefix}igcertificate
+  *≻* ${prefix}ytcertificate
+  `
+}
+
+exports.funmenu = (prefix) => {
+	return`
+  *GAME & FUN MENU*
+  *≻* ${prefix}simih
+  *≻* ${prefix}bagaimanakah
+  *≻* ${prefix}kapankah
+  *≻* ${prefix}apakah
+  *≻* ${prefix}bisakah
+  *≻* ${prefix}rate
+  *≻* ${prefix}wangy
+  *≻* ${prefix}gantengcek
+  *≻* ${prefix}cekganteng
+  *≻* ${prefix}cantikcek
+  *≻* ${prefix}cekcantik
+  *≻* ${prefix}sangecek
+  *≻* ${prefix}ceksange
+  *≻* ${prefix}gaycek
+  *≻* ${prefix}cekgay
+  *≻* ${prefix}lesbicek
+  *≻* ${prefix}halah
+  *≻* ${prefix}hilih
+  *≻* ${prefix}huluh
+  *≻* ${prefix}heleh
+  *≻* ${prefix}holoh
+  *≻* ${prefix}jadian
+  *≻* ${prefix}jodohku
+  *≻* ${prefix}tictactoe
+  *≻* ${prefix}delttt
+  *≻* ${prefix}family100
+  *≻* ${prefix}tebak
+  *≻* ${prefix}math
+  *≻* ${prefix}suitpvp [@tag]
+  `
+}
+
+exports.primbonmenu = (prefix) => {
+	return`
+  *PRIMBON MENU*
+  *≻* ${prefix}nomorhoki
+  *≻* ${prefix}artimimpi
+  *≻* ${prefix}artinama
+  *≻* ${prefix}ramaljodoh
+  *≻* ${prefix}ramaljodohbali
+  *≻* ${prefix}suamiistri
+  *≻* ${prefix}ramalcinta
+  *≻* ${prefix}cocoknama
+  *≻* ${prefix}pasangan
+  *≻* ${prefix}jadiannikah
+  *≻* ${prefix}sifatusaha
+  *≻* ${prefix}rezeki
+  *≻* ${prefix}pekerjaan
+  *≻* ${prefix}nasib
+  *≻* ${prefix}penyakit
+  *≻* ${prefix}tarot
+  *≻* ${prefix}fengshui
+  *≻* ${prefix}haribaik
+  *≻* ${prefix}harisangar
+  *≻* ${prefix}harisial
+  *≻* ${prefix}nagahari
+  *≻* ${prefix}arahrezeki
+  *≻* ${prefix}peruntungan
+  *≻* ${prefix}weton
+  *≻* ${prefix}karakter
+  *≻* ${prefix}keberuntungan
+  *≻* ${prefix}memancing
+  *≻* ${prefix}masasubur
+  *≻* ${prefix}zodiak
+  *≻* ${prefix}shio
+  `
+}
+
+exports.cerpenmenu = (prefix) => {
+	return`
+  *CERPEN MENU*
+  *≻* ${prefix}cerpen anak
+  *≻* ${prefix}cerpen bahasa daerah
+  *≻* ${prefix}cerpen bahasa inggris
+  *≻* ${prefix}cerpen bahasa jawa
+  *≻* ${prefix}cerpen bahasa sunda
+  *≻* ${prefix}cerpen budaya
+  *≻* ${prefix}cerpen cinta
+  *≻* ${prefix}cerpen cinta islami
+  *≻* ${prefix}cerpen cinta pertama
+  *≻* ${prefix}cerpen cinta romantis
+  *≻* ${prefix}cerpen cinta sedih
+  *≻* ${prefix}cerpen cinta segitiga
+  *≻* ${prefix}cerpen cinta sejati
+  *≻* ${prefix}cerpen galau
+  *≻* ${prefix}cerpen gokil
+  *≻* ${prefix}cerpen inspiratif
+  *≻* ${prefix}cerpen jepang
+  *≻* ${prefix}cerpen kehidupan
+  *≻* ${prefix}cerpen keluarga
+  *≻* ${prefix}cerpen kisah nyata
+  *≻* ${prefix}cerpen korea
+  *≻* ${prefix}cerpen kristen
+  *≻* ${prefix}cerpen liburan
+  *≻* ${prefix}cerpen lingkungan
+  *≻* ${prefix}cerpen lucu
+  *≻* ${prefix}cerpen malaysia
+  *≻* ${prefix}cerpen mengharukan
+  *≻* ${prefix}cerpen misteri
+  *≻* ${prefix}cerpen motivasi
+  *≻* ${prefix}cerpen nasihat
+  *≻* ${prefix}cerpen nasionalisme
+  *≻* ${prefix}cerpen olahraga
+  *≻* ${prefix}cerpen patah hati
+  *≻* ${prefix}cerpen penantian
+  *≻* ${prefix}cerpen pendidikan
+  *≻* ${prefix}cerpen pengalaman pribadi
+  *≻* ${prefix}cerpen pengorbanan
+  *≻* ${prefix}cerpen penyesalan
+  *≻* ${prefix}cerpen perjuangan
+  *≻* ${prefix}cerpen perpisahan
+  *≻* ${prefix}cerpen persahabatan
+  *≻* ${prefix}cerpen petualangan
+  *≻* ${prefix}cerpen ramadhan
+  *≻* ${prefix}cerpen remaja
+  *≻* ${prefix}cerpen renungan
+  *≻* ${prefix}cerpen rindu
+  *≻* ${prefix}cerpen rohani
+  *≻* ${prefix}cerpen romantis
+  *≻* ${prefix}cerpen sastra
+  *≻* ${prefix}cerpen sedih
+  *≻* ${prefix}cerpen sejarah
+  *≻* ${prefix}cerpen slice of life
+  *≻* ${prefix}cerpen terjemahan
+  *≻* ${prefix}cerpen thriller
+`
+}
+
+exports.convertmenu = (prefix) => {
+	return`
+  *CONVERT MENU*
+  *≻* ${prefix}toimage
+  *≻* ${prefix}removebg
+  *≻* ${prefix}sticker
+  *≻* ${prefix}emojimix
+  *≻* ${prefix}tovideo
+  *≻* ${prefix}togif
+  *≻* ${prefix}tourl
+  *≻* ${prefix}tovn
+  *≻* ${prefix}tomp3
+  *≻* ${prefix}toaudio
+  *≻* ${prefix}ebinary
+  *≻* ${prefix}dbinary
+  *≻* ${prefix}styletext
+  `
+}
+
+exports.informationmenu = (prefix) => {
+	return`
+  *INFORMATION MENU*
+  *≻* ${prefix}merdeka-news 
+  *≻* ${prefix}kontan-news 
+  *≻* ${prefix}cnbc-news 
+  *≻* ${prefix}tribun-news 
+  *≻* ${prefix}indozone-news 
+  *≻* ${prefix}kompas-news 
+  *≻* ${prefix}detik-news 
+  *≻* ${prefix}daily-news 
+  *≻* ${prefix}inews-news 
+  *≻* ${prefix}okezone-news 
+  *≻* ${prefix}sindo-news 
+  *≻* ${prefix}tempo-news 
+  *≻* ${prefix}antara-news 
+  *≻* ${prefix}cnn-news 
+  *≻* ${prefix}fajar-news
+`
+}
+
+exports.mainmenu = (prefix) => {
+	return`
+  *MAIN MENU*
+  *≻* ${prefix}script
+  *≻* ${prefix}ping
+  *≻* ${prefix}runtime
+  *≻* ${prefix}speed
+  *≻* ${prefix}owner
+  *≻* ${prefix}menu
+  *≻* ${prefix}delete
+  *≻* ${prefix}infochat
+  *≻* ${prefix}quoted
+  *≻* ${prefix}listpc
+  *≻* ${prefix}listgc
+  *≻* ${prefix}listonline
+  `
+}
+
+exports.databasemenu = (prefix) => {
+	return`
+  *DATABASE MENU*
+  *≻* ${prefix}setcmd
+  *≻* ${prefix}listcmd
+  *≻* ${prefix}delcmd
+  *≻* ${prefix}lockcmd
+  *≻* ${prefix}addmsg
+  *≻* ${prefix}listmsg
+  *≻* ${prefix}getmsg
+  *≻* ${prefix}delmsg
+`
+}
+
+exports.anonymousmenu = (prefix) => {
+	return`
+  *ANONYMOUS CHAT*
+  *≻* ${prefix}anonymous
+  *≻* ${prefix}start
+  *≻* ${prefix}next
+  *≻* ${prefix}keluar
+  *≻* ${prefix}sendkontak
+  `
+}
+
+exports.islamicmenu = (prefix) => {
+	return`
+  *ISLAMIC MENU*
+  *≻* ${prefix}iqra
+  *≻* ${prefix}hadist
+  *≻* ${prefix}alquran
+  *≻* ${prefix}juzamma
+  *≻* ${prefix}tafsirsurah
+  `
+}
+
+exports.voicechargermenu = (prefix) => {
+	return`
+  *VOICE CHANGER*
+  *≻* ${prefix}bass
+  *≻* ${prefix}blown
+  *≻* ${prefix}deep
+  *≻* ${prefix}earrape
+  *≻* ${prefix}fast
+  *≻* ${prefix}fat
+  *≻* ${prefix}nightcore
+  *≻* ${prefix}reverse
+  *≻* ${prefix}robot
+  *≻* ${prefix}slow
+  *≻* ${prefix}tupai
+  `
+}
+
+exports.ownermenu = (prefix) => {
+	return`
+  *OWNER MENU*
+  *≻* ${prefix}react
+  *≻* ${prefix}chat
+  *≻* ${prefix}join
+  *≻* ${prefix}leave
+  *≻* ${prefix}block @user
+  *≻* ${prefix}unblock @user
+  *≻* ${prefix}bcgroup 
+  *≻* ${prefix}bcall 
+  *≻* ${prefix}setppbot
+  *≻* ${prefix}setexif
+  *≻* ${prefix}setmenu
+  `
+}
+
+exports.thanksto = () => {
+	return`
+  👑 *THANKS TO* 👑
+  *≻* DikaArdnt
+  *≻* ZeeoneOfc
+  *≻* FatihArridho
+  *≻* Yoga
+  *≻* Lexxy Official
+  *≻* Riy Botz
+  *≻* Deff
+  *≻* Sanzy
+  *≻* Nekel
+  *≻* Bagas Prdn
+  *≻* RamzC
+`
+}
